9de007f2ccfd873bf9337248067d69a8bcac6b4b
kavinraj981 <36730619+kavinraj981@users.noreply.github.com>
2018-02-23 14:58:23 +0530

    Update vowel_consonant.py

--- Beginner/vowel_consonant.py
+++ Beginner/vowel_consonant.py
@@ -1,1 +1,5 @@
-
+a = input("Enter a charactor: ")
+if a=='a' or a=='e' or a=='i' or a=='o' or a=='u'or a=='A' or a=='E' or a=='I' or a=='O' or a=='U' :
+  print("The given charactor {} is vowel".format(a))
+else:
+   print("The given charactor {} is consonant".format(a))
