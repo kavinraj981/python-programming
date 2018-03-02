f04a299a20ab3485ebd45e12a7d3cc08ebc64f3b
kavinraj981 <36730619+kavinraj981@users.noreply.github.com>
2018-03-02 14:36:41 +0530

    Create palidrom.py

---
+++ Beginner/palidrom.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a number: "))
+b=a
+rev=0
+whaile(a<=1000):
+    d=a%10
+    rev=rev*10+dig
+    a=a//10
+    
+ if(b=rev):
+     print("The given number is polindrom")
+ else:
+     print("The given number is not polindrom")
