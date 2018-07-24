c42d3528eee11df455d8e76998c68e47684a015c
kavinraj981 <36730619+kavinraj981@users.noreply.github.com>
2018-07-24 16:34:02 +0530

    Update factorial.py

--- Beginner/factorial.py
+++ Beginner/factorial.py
@@ -1,10 +1,4 @@
-a=int(input("Enter the Number: "))
-j=a
-k=a
-for i in range(0,a,1):
-    j=j-1
-    if j>0:
-       a=a*j
-print(k,"! =",a)
-    
-    
+import math
+a=int(input("Enter the Value: "))
+print(a,"! =",math.factorial(a))
+
